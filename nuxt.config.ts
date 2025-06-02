4bdebf0001f39e78b1d681ba5889be56d96f1fc3
Sophia Banda <sophia.m.banda@gmail.com>
2025-06-02 16:46:37 -0400

    Struggling my way through learning Drizzle, Turso and Nuxt and Typescript all at once - grimace

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  css: ["@picocss/pico"]
-})
+    compatibilityDate: "2025-05-15",
+    devtools: { enabled: true },
+    css: ["@picocss/pico"],
+    runtimeConfig: {
+        turso: {
+            databaseUrl: "",
+            authToken: "",
+        },
+    },
+});
